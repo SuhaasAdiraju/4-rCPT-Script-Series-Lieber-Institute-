9145a20e1fde264093370e2201a61a952b328eeb
Suhaas Adiraju <62865417+SuhaasAdiraju@users.noreply.github.com>
2021-12-17 17:10:31 -0500

    Event-type sliced transients structure (For Neurotransmitter sensors)
    This function is to grab neuro-transmitter sensor transients surrounding event timestamps. It is written assuming the user has followed NTsensors4CPT 1, and has the mouse structure with event timestamps saved as a distinct variable, along with the bio-sensor imaging, transients saved in a structure...The outcome will be transients sliced based on event-type and event timestamps

--- NTsensors_4CPT2.m
+++ NTsensors_4CPT2.m
@@ -1,36 +1,35 @@
-%% Grab NTransmitter Transients surrounding event timestamps
-% This script is written assuming the user has followed EFT 4 CPT 1, and
+function [] = sliceNTsensors(struc_path, struc_name, srate, TimeWin, saveplace, mousename)
+%% DESCRIPTION
+% This function is to grab neuro-transmitter sensor transients surrounding event timestamps
+% This function is written assuming the user has followed NTsensors4CPT 1, and
 % has the mouse structure with event timestamps saved as a distinct
-% variable, along with bio-sensor imaging,
-% transients saved in a structure
+% variable, along with the bio-sensor imaging, transients saved in a structure
 
 % - Written by Suhaas S Adiraju
 
 %%
 % cd to location of of the structure
-cd('Z:\Circuits projects (CPT)\CPT Recording Data\GRAB_NE'); % ('yourpath')
+cd(struc_path{1});
 
 % load file of choice
-load ("GRAB_NE_1640.mat")
+load (struc_name{1})
+
+Transients = Ne_transients;
 
 %% Trimming our signal (optional)
-%{
-% What's your sampling rate
-srate = srate;
 
 % Set transients on the proper timescale, ("give me a vector of 0-total
 % seconds in value, but with a length of the total # of samples in the original matrix")
 Transients_TimeVec = linspace(0, ((length(Transients))/srate), (length(Transients)));
-
- 
 
 % you can see right away that the transients length is too long, and we
 % know the cpt schedule is only 1800s (30min), and we know that our
 % 0seconds in the signal is true zero of the cpt task cause we use a TTL
 % so we can trim the transients signal to the length of the cpt
 cpt_length = Transients_TimeVec(Transients_TimeVec <= 1800);
-int64(cpt_length)= cpt_length .*1000
-(FIRBeam_On)
+cpt_transients = Transients(:,[1:(length(cpt_length))]);
+
+%ylabel('dF/F (change in fluorescent expression)')
 %}
 %% Equalizing Resolutions
 
@@ -38,11 +37,10 @@ int64(cpt_length)= cpt_length .*1000
 % Transients = Transients(\);
 
 
-% First, we must up-sample, to match the scale(resolution) at which we have Ne_transients
+% First, we must up-sample, to match the scale(resolution) at which we have Transients
 % i.e. event timestamps 
 FIRBeam_Onidx = FIRBeam_On * srate; 
 FIRBeam_Onidx =int64(FIRBeam_Onidx); % here im converting the class of the IDX values because as a double certain vals were not integers (ie 10385.00000 became 1.0385x10e6)
-Transients(FIRBeam_Onidx);
 
 
 FIRBeam_Offidx = FIRBeam_Off * srate; 
@@ -69,74 +67,81 @@ Correct_Rejidx = int64(Correct_Rejidx);
 False_Alarmidx = False_Alarm * srate; 
 False_Alarmidx = int64(False_Alarmidx);
 
-%% Now we can grab the Ne_transients of each timestamp
+%% Now we can grab the Transients of each timestamp
 
 % Make it an if... statement, so that when we have empty variables (S2 as
 %one case) then we can still run the full script and wont be stopped by
 %errors
+
 if sum(size(FIRBeam_On)) >= 2 
     for i = 1:length(FIRBeam_Onidx)
-        if (FIRBeam_Onidx(i)+(srate*2))<(length(Ne_transients)) && (FIRBeam_Onidx(i)-(srate*2))>(0)  
-            NeTransients.FIRBeam_On{i} = Ne_transients(1,[FIRBeam_Onidx(1,i)-(srate*2):FIRBeam_Onidx(1,i)+(srate*2)]);
+        if (FIRBeam_Onidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (FIRBeam_Onidx(i)-(srate*TimeWin{1}))>(0)  
+            CutTransients.FIRBeam_On_transients{i} = Transients(1,[FIRBeam_Onidx(1,i)-(srate*TimeWin{1}):FIRBeam_Onidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 
 if sum(size(FIRBeam_Off)) >= 2 
     for i = 1:(length(FIRBeam_Offidx))
-        if (FIRBeam_Offidx(i)+(srate*2))<(length(Ne_transients)) && (FIRBeam_Offidx(i)-(srate*2))>(0)        
-            NeTransients.FIRBeam_Off_Ne_transients{i} = Ne_transients(1,[FIRBeam_Offidx(1,i)-(srate*2):FIRBeam_Offidx(1,i)+(srate*2)]);
+        if (FIRBeam_Offidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (FIRBeam_Offidx(i)-(srate*TimeWin{1}))>(0)        
+            CutTransients.FIRBeam_Off_Transients{i} = Transients(1,[FIRBeam_Offidx(1,i)-(srate*TimeWin{1}):FIRBeam_Offidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 
 if sum(size(Center_ScTouch)) >= 2 
     for i = 1:length(Center_ScTouchidx)
-        if (Center_ScTouchidx(i)+(srate*2))<(length(Ne_transients)) && (Center_ScTouchidx(i)-(srate*2))>(0)              
-            NeTransients.Center_ScTouch_Ne_transients{i} = Ne_transients(1,[Center_ScTouchidx(1,i)-(srate*2):Center_ScTouchidx(1,i)+(srate*2)]);
+        if (Center_ScTouchidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Center_ScTouchidx(i)-(srate*TimeWin{1}))>(0)              
+            CutTransients.Center_ScTouch_Transients{i} = Transients(1,[Center_ScTouchidx(1,i)-(srate*TimeWin{1}):Center_ScTouchidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 
 if sum(size(Start_ITI)) >= 2 
     for i = 1:length(Start_ITIidx)
-        if (Start_ITIidx(i)+(srate*2))<(length(Ne_transients)) && (Start_ITIidx(i)-(srate*2))>(0)                        
-            NeTransients.Start_ITI_Ne_transients{i} = Ne_transients(1,[Start_ITIidx(1,i)-(srate*2):Start_ITIidx(1,i)+(srate*2)]);
+        if (Start_ITIidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Start_ITIidx(i)-(srate*TimeWin{1}))>(0)                        
+            CutTransients.Start_ITI_Transients{i} = Transients(1,[Start_ITIidx(1,i)-(srate*TimeWin{1}):Start_ITIidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 if sum(size(Stimulus)) >= 2 
     for i = 1:length(Stimulusidx)
-        if (Stimulusidx(i)+(srate*2))<(length(Ne_transients)) && (Stimulusidx(i)-(srate*2))>(0)                      
-            NeTransients.Stimulus_Ne_transients{i} = Ne_transients(1,[Stimulusidx(1,i)-(srate*2):Stimulusidx(1,i)+(srate*2)]);
+        if (Stimulusidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Stimulusidx(i)-(srate*TimeWin{1}))>(0)                      
+            CutTransients.Stimulus_Transients{i} = Transients(1,[Stimulusidx(1,i)-(srate*TimeWin{1}):Stimulusidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 if sum(size(Hit)) >= 2
     for i = 1:length(Hitidx)
-        if (Hitidx(i)+(srate*2))<(length(Ne_transients)) && (Hitidx(i)-(srate*2))>(0)                        
-            NeTransients.Hit_Ne_transients{i} = Ne_transients(1,[Hitidx(1,i)-(srate*2):Hitidx(1,i)+(srate*2)]);
+        if (Hitidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Hitidx(i)-(srate*TimeWin{1}))>(0)                        
+            CutTransients.Hit_Transients{i} = Transients(1,[Hitidx(1,i)-(srate*TimeWin{1}):Hitidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end    
 if sum(size(Miss)) >= 2
     for i = 1:length(Missidx)
-        if (Missidx(i)+(srate*2))<(length(Ne_transients)) && (Missidx(i)-(srate*2))>(0)               
-            NeTransients.Miss_Ne_transients{i} = Ne_transients(1,[Missidx(1,i)-(srate*2):Missidx(1,i)+(srate*2)]);
+        if (Missidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Missidx(i)-(srate*TimeWin{1}))>(0)               
+            CutTransients.Miss_Transients{i} = Transients(1,[Missidx(1,i)-(srate*TimeWin{1}):Missidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 if sum(size(Correct_Rej)) >= 2
     for i = 1:length(Correct_Rejidx)
-        if (Correct_Rejidx(i)+(srate*2))<(length(Ne_transients)) && (Correct_Rejidx(i)-(srate*2))>(0)                      
-            NeTransients.Correct_Rej_Ne_transients{i} = Ne_transients(1,[Correct_Rejidx(1,i)-(srate*2):Correct_Rejidx(1,i)+(srate*2)]);
+        if (Correct_Rejidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (Correct_Rejidx(i)-(srate*TimeWin{1}))>(0)                      
+            CutTransients.Correct_Rej_Transients{i} = Transients(1,[Correct_Rejidx(1,i)-(srate*TimeWin{1}):Correct_Rejidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
 if sum(size(False_Alarm)) >= 2
     for i = 1:length(False_Alarmidx)
-        if (False_Alarmidx(i)+(srate*2))<(length(Ne_transients)) && (False_Alarmidx(i)-(srate*2))>(0)                     
-            NeTransients.False_Alarm_Ne_transients{i} = Ne_transients(1,[False_Alarmidx(1,i)-(srate*2):False_Alarmidx(1,i)+(srate*2)]);
+        if (False_Alarmidx(i)+(srate*TimeWin{1}))<(length(Transients)) && (False_Alarmidx(i)-(srate*TimeWin{1}))>(0)                     
+            CutTransients.False_Alarm_Transients{i} = Transients(1,[False_Alarmidx(1,i)-(srate*TimeWin{1}):False_Alarmidx(1,i)+(srate*TimeWin{1})]);
         end
     end
 end
+
+%% Can save this structure of sliced transients as well or continue straight into script #3
+cd(saveplace{1});
+save(mousename{1}, '-struct', 'savename')
+end
+
