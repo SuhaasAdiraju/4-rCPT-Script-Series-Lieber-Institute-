b7fd7fc9d04e2631120f6b6d80357aa7375e6793
Suhaas Adiraju <62865417+SuhaasAdiraju@users.noreply.github.com>
2021-12-28 14:46:23 -0500

    Average Response Patterns

--- Ca2imaging_4CPT3.m
+++ Ca2imaging_4CPT3.m
@@ -1,6 +1,7 @@
-%% Basic analysis with behavior-surrounding Ca2 transients 
-% This script is beginning the basic analyses with CA2 imaging such as
-% collapsing across events to yield an average response surrounding an event type;
+%% DESCRIPTION
+% Basic analysis with behavior-surrounding Ca2 transients 
+% This function begins the basic analyses with CA2 imaging such as
+% collapsing across trials;
 
 % Written assuming the user has:
     % 1) followed Ca2imaging 4 CPT 1, saved a
@@ -9,22 +10,31 @@
     % 2) followed Ca2imaging 4 CPT 2, produced the 'CutTransients' structure,
     % and now is coming over to start analysis using this script 
 
-% This also uses the example of the Event "Hits", but can be implemented
-% for any event type
+% You may simply 'Run' this script (editor tab top left). After input of strucpath and strucname
+% Or run piece by piece if trying to learn
 
 % - Written by Suhaas S Adiraju
 
-%% Load in our existing data to work with 
-% (for now the data is too large, so just complete script 2, and move to script 3, dont try to save and reload)
+%% Define and load your previously saved structure (data set)
+strucpath = {'Z:\Circuits projects (CPT)\CPT Recording Data\EXAMPLE SCRIPTS SAMPLE DATA'};
+strucname = {'S3Good_1855_Ca2'}; % enter the name of the structure
 
-%cd('Z:\Circuits projects (CPT)\CPT Recording Data\GCaMP6f');
-%load ('CutTransients.mat') %load your structure
+cd(strucpath{1});
+load (strucname{1}) %load your structure
+
+
+%% What event type would you like to look at?
+
+vars = who;
+sprintf('%s\n',vars{:})
+x = input ('Above includes display of the event types. What event type would you like to look at?(Type with no quotes)\n');
+Event_Type = x; 
+
 
 %% For calcium imaging, something we want is to average across events and neurons
 % just for a reminder, identify your window of analysis 
-win = length(CutTransients.Hit_Transients{1})
-win/2
-% the sampling rate was ~30, so we have 4 seconds of transient before and after each hit
+TimeWin
+
 
 % Now i want to average across hit events, and have a vector of one
 % averaged hit 'event', so add up each timestamp-based transient across
@@ -34,13 +44,13 @@ win/2
 % a specific category...like 'the first hit timestamp transient value for 1 neuron, all events'
 
 % so we grab the first timestamp transient value of all events for 1 neuron like so 
-   for eventi = 1:length(CutTransients.Hit_Transients(1,:))
-       exampleCellStamps(1,eventi) = CutTransients.Hit_Transients{1,eventi}{1,1};
+   for eventi = 1:length(Event_Type(1,:))
+       exampleCellStamps(1,eventi) = Event_Type{1,eventi}{1,1};
    end
 
 %% Sanity check, do things match-up? 
 % we will take the 1st timestamp of the 1st neuron 3rd event (arbitrary testing)
-test1 = CutTransients.Hit_Transients{1,3}{1,1};
+test1 = Event_Type{1,3}{1,1};
 
 % and now compare it to the same neuron same event of the example cell
 % stamps variable we made
@@ -51,25 +61,25 @@ else
        disp('Somethings up here...try to check your work')
 end
 
-% if yes, lets move forward, and we have a 1neuron example to check our
+% if yes, lets move forward, and we have a 1 neuron example to check our
 % full matrix with!
 
 
 %% Using our example as a reference, lets now work on it for all events all timestamps
 
 % This may help if the syntax is really confusing here
-% CutTransients.Hit_Transients is organized as such
-    % CutTransients.Hit_Transients(neuron,event#){8 second window of timestamp based transients}
+% Event_Type is organized as such
+    % Event_Type(neuron,event#){8 second window of timestamp based transients}
 
 % this gets quite confusing, the key is understanding how your data being 
 % used is organized, refer above for some help with this
 
 % Every neuron, every transient, organized by timestamp, for every event
     % so cell array is neuron, row is timestamp, and column is event 
-for neuroni = 1:length(CutTransients.Hit_Transients(:,1))
-    for eventi = 1:length(CutTransients.Hit_Transients(1,:))
-        for stampi = 1:length(CutTransients.Hit_Transients{1,1})
-           AllCell_array{neuroni}(stampi,eventi) = CutTransients.Hit_Transients{neuroni,eventi}{1,stampi};
+for neuroni = 1:length(Event_Type(:,1))
+    for eventi = 1:length(Event_Type(1,:))
+        for stampi = 1:length(Event_Type{1,1})
+           AllCell_array{neuroni}(stampi,eventi) = Event_Type{neuroni,eventi}{1,stampi};
            
         end
     end
@@ -83,7 +93,7 @@ end
 % We can verify that the first column of our All Cell matrix 
 % (all timestamp associated transients in the defined window for each event) is correctly brought over
 
-% so within the CutTransients.Hit_Transients(1,1), that's our original, and representative of 8 seconds
+% so within the Event_Type(1,1), that's our original, and representative of 8 seconds
 % of transient from one neuron for 1 event
 
 % AllCell_array(1,1){:,1}, or within one cell array(neuron), all rows in the first column (first event) (8 second timewindow)
@@ -98,33 +108,29 @@ end
 
 %% Test 1; Neuron 1//Transients of the 8 second time window//For Event #1
 testAllCell = cell2mat(AllCell_array(1,1)); % first neuron, all events all 8 second windows
-testOG = cell2mat(CutTransients.Hit_Transients{1,1}); % first neuron, first event's 8 second window
+testOG = cell2mat(Event_Type{1,1}); % first neuron, first event's 8 second window
 
 if testOG == (testAllCell(:,1)')
     sprintf('Because our 1st event-based transients for Neuron 1 saved in our new structure,\n(AllCell_array) \nand our original 1st event transients match...\nWere good to go!')
 else 
     sprintf('Something is off between your original transients x (neuron,event) array and your new transients x (timestamps,event) array')
-    sprintf('Press any key to continue')
 end
 
 
-pause
 clear testAllCell testOG
 
 %% Test 2; Neuron 1//Transients of the 8 second time window//For Event #2
 testAllCell = cell2mat(AllCell_array(1,1)); % first neuron, all events all 8 second windows
-testOG = cell2mat(CutTransients.Hit_Transients{1,2}); % first neuron, SECOND event's 8 second window
+testOG = cell2mat(Event_Type{1,2}); % first neuron, SECOND event's 8 second window
 
 if testOG == (testAllCell(:,2)')
     sprintf('Because our 2nd event-based transients for Neuron 1 saved in our new structure,\n (AllCell_array) \nand our original 2nd event-based time window transients match...\nWere good to go!')
     sprintf('Press any key to continue')
 else 
     sprintf('Something is off between your original transients x (neuron,event) array and your new transients x (timestamps,event) array')
-    sprintf('Press any key to continue')
 end
 
 
-pause
 clear testAllCell testOG
 
 
@@ -132,19 +138,15 @@ clear testAllCell testOG
 % (to make it arbitrary for safety)
 % 
 testAllCell = cell2mat(AllCell_array(1,2)); % Now second neuron, all events all 8s time windows
-testOG = cell2mat(CutTransients.Hit_Transients{2,3}); % Now 2nd neuron, 3rd event's 8 second window
+testOG = cell2mat(Event_Type{2,3}); % Now 2nd neuron, 3rd event's 8 second window
 
 if testOG == (testAllCell(:,3)') %because we took all events all windows from AllCell, the column # here indicates what event to assess
     sprintf('Because our 3rd event-based transients for Neuron 1 grabbed from our original structure,\n (saved in AllCell_array) \nand our original 3rd event-based transients match...\nWere good to go!')
-    sprintf('Press any key to continue')
 else 
     sprintf('Something went wrong here...')
-    sprintf('Press any key to continue')
-
 end
 
 
-pause
 clear testAllCell testOG
 %% Summing the transients
 % Now we can simply use the matlab sum(A) function, which sums across the
@@ -164,49 +166,90 @@ end
 %% A couple sanity checks here too!
 
 % Test 1, the length of our Averaged Hit Window
-if length(AvgNeuronWins{1}) == length(CutTransients.Hit_Transients{1})
+if length(AvgNeuronWins{1}) == length(Event_Type{1})
     sprintf('\nYour original array neuron 1 event one window length\nand the length of your neuron 1 averaged window match!')
-    sprintf('Press any key to continue')
 else
     sprintf('\nyou need to check out your averaging step, the length of your average window is incorrect')
-    sprintf('Press any key to continue')    
 end
 
-pause
 
 % 2, is using our handy example average! 
 % Remember, we made the average of timestamp 1 associated transients, across all the events, for Neuron 1
 % So...
 if exampleAvg == AvgNeuronWins{1,1}(1,1)
     sprintf('\nBecause your 1st timestamp average across events (for Neuron 1) \nmatches between your original example average \nand your All cell averaging,\nYour AllCell array looks good!')
-    sprintf('Press any key to continue')
 else
     sprintf('\nyou need to check out your averaging step,\nyour first timestamp across events for neuron 1 average,\nand your original example don''t match')
-    sprintf('Press any key to continue')
 end
-pause
 
 
 %% Plotting
 
 
-% this plots the averaged across events traces for every neuron 
-srate = 30.0136; % for now 
-TimeWin = 4;
-hitlength = linspace(-(length(CutTransients.Hit_Transients{1})/(srate*(TimeWin/2))),(length(CutTransients.Hit_Transients{1})/(srate*(TimeWin/2))),length(CutTransients.Hit_Transients{1}));
-% Check it out
+% this plots the averaged across-events traces for every neuron
+
+hitlength = linspace(-(length(Event_Type{1})/(srate*(TimeWin/2))),(length(Event_Type{1})/(srate*(TimeWin/2))),length(Event_Type{1}));
+
+% All neurons together (Non Normalized)
+
 for neuroni = 1:length(AvgNeuronWins)
-    figure; 
-    plot(hitlength,AvgNeuronWins{neuroni}, 'k', 'LineWidth',1)
-    xlabel('Time(s)')
-    ylabel('dF/F')
-    xlim ([-4 4])
-    ylim ([-10 20])
-    title(sprintf('Neuron %d, Hit Averaged Activity',neuroni))
+    if neuroni ==1 
+    figure;
+    end 
+        plot(hitlength, AvgNeuronWins{neuroni}); hold on;
+        %plot(hitlength, AvgNeuronWins{neuroni}) %'k', 'LineWidth',1)
+        xlabel(sprintf('Time(%s s)', TimeWin))
+        ylabel('dF/F')
+        xlim ([-TimeWin TimeWin])
+        %ylim ([-10 40])
+        title(sprintf('All Neuron Activity Traces (non-normalized)'))
 end
 
-% save the workspace to be able to easily plot in the future 
-save('Ca2exampleAllCellsAvg')
+
+% Individual neurons
+%{
+for neuroni = 1:length(AvgNeuronWins)
+        figure;
+        plot(hitlength, AvgNeuronWins{neuroni},'k', 'LineWidth',1)
+        xlabel(sprintf('Time(%s s)', TimeWin))
+        ylabel('dF/F')
+        xlim ([-TimeWin TimeWin])
+        ylim ([-10 40])
+        title(sprintf('Neuron %d, Hit Averaged Activity',neuroni))
+end
+%}
+
+disp('This is cool, but if you look at the baselines they''re all over the place which doesnt make total sense,\nRather, we need to correct for this...')
+
+%% Normalizing a baseline
+
+% we can normalize a baseline by finding the meanval df/f for each average time window for each neuron and
+% then subtracting that mean val from each mean timestamp
+
+for neuroni = 1:length(AvgNeuronWins)
+    % find the meanval for each neuron
+    NeuronMeanval{neuroni} = mean(AvgNeuronWins{neuroni},2); 
+    % subtract meannval from avg window trace values
+    NormalizedNeuronWins{neuroni} = (AvgNeuronWins{neuroni} - NeuronMeanval{neuroni});
+end
+
+
+%% Re-examine Results 
+% lets plot all traces again with normalization
+for neuroni = 1:length(AvgNeuronWins)
+    if neuroni ==1 
+    figure;
+    end 
+        plot(hitlength, NormalizedNeuronWins{neuroni}); hold on;
+        %plot(hitlength, AvgNeuronWins{neuroni}) %'k', 'LineWidth',1)
+        xlabel(sprintf('Time(%s s)', TimeWin))
+        ylabel('dF/F')
+        xlim ([-TimeWin TimeWin])
+        %ylim ([-10 40])
+        title(sprintf('All Neuron Activity Traces'))
+end
+
+
 
 %% Directions to consider
 
@@ -214,4 +257,4 @@ save('Ca2exampleAllCellsAvg')
 % starters, thresholding averages, and identifying a few common motifs
 
 % If we move out of just hits, we can model neural activity predicting
-% stimulus (i think this is possible in calcium imaging 
+% stimulus...
